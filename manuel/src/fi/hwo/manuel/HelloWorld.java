8a5f8172c224ae5fabf5cf47d7b3ee806a44499f
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2012-10-04 12:22:41 +0300

    ignores

---
+++ manuel/src/fi/hwo/manuel/HelloWorld.java
@@ -0,0 +1,11 @@
+package fi.hwo.manuel;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: plouh
+ * Date: 4.10.2012
+ * Time: 12.20
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+}
