e64df47e51628a69e9f9a8f1c84e0c3ac06ca5c1
Petri Louhelainen <petri.louhelainen@reaktor.fi>
2012-10-18 00:23:47 +0300

    Splast screen

--- manuel/src/fi/hwo/manuel/HelloWorld.java
+++ manuel/src/fi/hwo/manuel/HelloWorld.java
@@ -1,11 +1,36 @@
 package fi.hwo.manuel;
 
-/**
- * Created with IntelliJ IDEA.
- * User: plouh
- * Date: 4.10.2012
- * Time: 12.20
- * To change this template use File | Settings | File Templates.
- */
-public class HelloWorld {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+public class HelloWorld extends Activity {
+
+  protected int _splashTime = 5000;
+
+  private Thread splashTread;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.splash);
+
+    splashTread = new Thread() {
+      @Override
+      public void run() {
+        try {
+          synchronized(this){
+            wait(_splashTime);
+          }
+        } catch(InterruptedException e) {}
+        finally {
+          finish();
+          startActivity(new Intent(HelloWorld.this, Login.class));
+        }
+      }
+    };
+
+    splashTread.start();
+  }
 }
